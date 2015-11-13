92f78a3dd758d43117acfead808afa49b9f9e313
angiemartaus <angelamartaus@gmail.com>
2015-11-13 11:43:24 -0500

    Added flash notice to create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "Thanks for signing up!"
       redirect_to root_path
     else
       render :new
