f3090c4dab75d496cfc00dfebba5b2fbb8ec7b8d
angiemartaus <angelamartaus@gmail.com>
2015-11-13 00:59:13 -0500

    Added .DS_Store to .gitignore, created user model & migration to add properties to users, started skeleton of user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
