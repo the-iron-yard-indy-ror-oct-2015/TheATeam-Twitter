cc6473b5d614800ffbeda5b587c5be36df161327
angiemartaus <angelamartaus@gmail.com>
2015-11-15 12:03:00 -0500

    A few tweaks, maybe none

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def self.sort_by_users
-    Post.all.sort_by{|x| x.users.count}.reverse
+   Post.all.sort_by{|x| x.users.count}.reverse
   end
 
   validates :post_message, presence: true, length: {maximum: 170}
