0bdbef4e9507f0e9f3bbf5eb72c1c460546b5d4f
angiemartaus <angelamartaus@gmail.com>
2015-11-17 21:01:28 -0500

    Cleaned up posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,59 +1,32 @@
 class PostsController < ApplicationController
 
   def index
-
-
     @posts = Post.order("created_at DESC").page(params[:page]).per(10)
-
-
-    @posts = Post.order("created_at DESC").page(params[:page]).per(10)
-
   end
 
-  def create
-    @post = Post.new(post_params)
-end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-def show
-  @post = Post.find(params[:id])
-
-end
-
-def new
-  @post = Post.new
-end
+  def new
+    @post = Post.new
+  end
 
   def create
     @post = Post.new(post_params)
       if @post.save
-        format.html { redirect_to post_path}
+        flash[:success] = "Thanks for sharing!"
         redirect_to root_path
       else
-        flash[:notice] = "Message failed to save"
+        flash[:notice] = "Message failed to save."
         render :new
       end
     end
 
+  private
 
-
-def update
-
+  def post_params
+    params.require(:post).permit(:message)
   end
 
-private
-
-def set_post
-      @post = Post.find(params[:id])
-
-    end
-
-def post_params
-  params.require(:post).permit(:user_name, :url, :message)
-end
-def check_if_current_user_is_owner
-  @post = Post.find(params[:id])
-  unless @post.user == @current_user
-    redirect_to root_url
-  end
-end
 end
