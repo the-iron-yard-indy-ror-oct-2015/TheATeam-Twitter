aa3088a8038b80a2d0f639b78fc73b983badd7c8
angiemartaus <angelamartaus@gmail.com>
2015-11-18 01:52:14 -0500

    Logged in users see only their own posts and posts by those they follow

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def self.sort_by_users
-   Post.all.sort_by{|x| x.users.count}.reverse
+    Post.all.sort_by{|x| x.users.count}.reverse
+  end
+
+  def self.timeline(user)
+    following_ids = user.all_following.map(&:id)
+    all_ids= following_ids << user.id
+    Post.where(user_id: all_ids).order("created_at DESC")
   end
 
   validates :message, presence: true, length: {maximum: 170}
